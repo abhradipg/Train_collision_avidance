8b1eb60915f7685e28225565c66d02ec9c54b6b9
abhradipg <abhradip.ghosh9@gmail.com>
2023-11-14 22:12:09 +0530

    ack

---
+++ temp.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import time
+
+# Get the current time in seconds since the epoch
+current_time_seconds = time.time()
+
+# Print the current time
+print("Current Time (in seconds since the epoch):", current_time_seconds)
+# Get the current date and time
+current_time = datetime.now()
+
+# Print the current time
+print("Current Time:", current_time)
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 def main():
     server_ip = '10.217.59.110'
@@ -8,8 +9,8 @@ def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     while True:
-        # Get user input
-        message = input("Enter message to send (type 'exit' to quit): ")
+        # Get user input (message with a number)
+        message = input("Enter message with a number to send (type 'exit' to quit): ")
 
         if message.lower() == 'exit':
             break
@@ -17,9 +18,25 @@ def main():
         # Send the message to the server
         client_socket.sendto(message.encode(), (server_ip, server_port))
 
-        # Receive acknowledgment from the server
-        ack_data, server_address = client_socket.recvfrom(1024)
-        print(f"Acknowledgment from server {server_address}: {ack_data.decode()}")
+        # Set a timeout for the acknowledgment
+        client_socket.settimeout(1.0)
+
+        try:
+            # Receive acknowledgment from the server
+            ack_data, server_address = client_socket.recvfrom(1024)
+            ack_number = int(ack_data.decode().split()[-1])  # Extract the number from the acknowledgment
+            print(f"Acknowledgment from server {server_address}: {ack_data.decode()}")
+
+            # Check if the acknowledgment number matches the sent number
+            if int(message.split()[-1]) == ack_number:
+                print("Acknowledgment received successfully.")
+            else:
+                print("Received acknowledgment with wrong number. Resending the message.")
+                continue
+        except socket.timeout:
+            # Handle timeout (resend the message)
+            print("Timeout! Resending the message.")
+            continue
 
     # Close the socket
     client_socket.close()
