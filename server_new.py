d35e19d51eba2561e5a36cf8fb7b181ba1ebec4a
Gulshan Gabel <gulshangabel@Gulshans-MacBook-Air.local>
2023-11-20 19:44:54 +0530

    changes to server code

--- server_new.py
+++ server_new.py
@@ -11,8 +11,35 @@ import time
 
 #receive data from trains and send acknowldgement back and update train-track table
 def receiver(train_table,queue):
-    
-    return 0
+    while True:
+        data, client_address = server_socket.recvfrom(1024)
+        data = pickle.loads(data)
+        queue.put(data)
+        track_id = data[1] 
+        ip_addr, port_no = client_address
+        train_id = data[3]
+        ack_no = data[-1]
+
+        for key in train_table:
+            if key != track_id:
+                tuple_list = train_table[key]
+                for tuple in tuple_list:
+                    if tuple[0] == train_id:
+                        train_table.remove(tuple)
+
+        tuple = [train_id,ip_addr,port]
+        if train_id in train_table:
+            # Key already exists, append value to the existing list
+            if tuple not in train_table[key]:
+                train_table[key].append(tuple)
+        else:
+            # Key does not exist, create a new entry with a list containing the value
+            train_table[key] = [tuple]
+
+        print(f"Updated table: {train_table}")
+
+        # Send data to each value in the list associated with the key using UDP
+        send_udp_data(ip_addr, ack_no)
 
 #send data to trains in current tracks and wait for acks back
 def sender(train_table,queue):
