d8eb00ada8de6792172dcddecc524c6952c7be18
abhradipg <abhradip.ghosh9@gmail.com>
2023-11-21 22:54:43 +0530

    print

--- train_new.py
+++ train_new.py
@@ -139,9 +139,11 @@ def receiver(shared_gps,curr_ack,received_ack,ack_lock,lock):
                 received_ack=1
             ack_lock.release()
         else:
-            ack_message = [segments[-1]]
+            ack_message= []
+            ack_message = ack_message.append(segments[-1])
             ack_message = ack_message.append(train_id)
             print("train ack")
+            print(segments[-1])
             print(ack_message)
             ack_message = pickle.dumps(ack_message)
             client_ip, client_port = client_address
