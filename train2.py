c2acff0180e503743f618c5bcb929f0da6643d90
abhradipg <abhradip.ghosh9@gmail.com>
2023-11-22 13:47:27 +0530

    sdf

--- train2.py
+++ train2.py
@@ -17,7 +17,7 @@ my_dict = {}
 def rfid_reader(queue,slowdown,speed_lock):
     my_dict=generate_dict.generate_dict()
     old_data=''
-    file_path = 'dummy_data2.txt'
+    file_path = 'dummy_data1.txt'
     #while True:
     with open(file_path, 'r') as file:
         for line in file:
@@ -40,14 +40,14 @@ def rfid_reader(queue,slowdown,speed_lock):
                 old_data=data
                 if data in my_dict:
                     queue.put(my_dict[data])
-                    print("put data", data)
+                    #print("put data", data)
                 else:
                     # Handle the case where data is not in my_dict
                     print(f"Data {data} not found in my_dict")
 
 
 def sender(queue,shared_gps,curr_ack,received_ack,ack_lock,lock):
-    server_ip = '10.217.58.125'
+    server_ip = '10.114.241.211'
     server_port = 2000
     server_addr = (server_ip, server_port)
 
@@ -63,7 +63,7 @@ def sender(queue,shared_gps,curr_ack,received_ack,ack_lock,lock):
     ack_lock.release()
     rtt_approx=4
     speed=100
-    train_id=12340
+    train_id=12345
     curr_time=0
     send_time=0
     ack_no=0
@@ -73,7 +73,7 @@ def sender(queue,shared_gps,curr_ack,received_ack,ack_lock,lock):
         try:
             queue_empty=0
             data = queue.get(block=False)
-            print("received new data")
+            print("read new rfid")
             lock.acquire()
             shared_gps[0]=data[0][0]
             shared_gps[1]=data[0][1]
@@ -91,8 +91,8 @@ def sender(queue,shared_gps,curr_ack,received_ack,ack_lock,lock):
             received_ack.value=0
             ack_lock.release()
             send_time=time.time()
-            print("sending data")
-            print(data)
+            print("sending rfid data")
+            #print(data)
             data = pickle.dumps(data)
             client_socket.sendto(data, (server_ip, server_port))
         
@@ -100,8 +100,8 @@ def sender(queue,shared_gps,curr_ack,received_ack,ack_lock,lock):
             curr_time=time.time()
             if curr_time - send_time > rtt_approx:
                 send_time = time.time()
-                print("resending data")
-                print(data)
+                print("Ack not received resending data")
+                #print(data)
                 client_socket.sendto(data, (server_ip, server_port))
 
 def process_data(lat1, lon1, lat2, lon2):
@@ -119,7 +119,7 @@ def process_data(lat1, lon1, lat2, lon2):
 
     # Calculate the distance
     distance = radius * c
-    print("distance is - "+str(distance))
+    #print("distance is - "+str(distance))
     return distance
 
 def print_metrics(segments,distance,speed_lock):
@@ -132,12 +132,12 @@ def print_metrics(segments,distance,speed_lock):
     print("Train ID: ",str(train_id)," is at location: ",str(segments[0][0]),"\u00B0 N ",str(segments[0][1]),"\u00B0 E")
     print("Speed of train is: ",str(speed),"km/hr \n")
     print("Distance between trains is: ",str(distance),"km \n")
-    if distance < 0.35:
+    if distance < 0.300:
         speed_lock.acquire()
         slowdown.value = 2
         speed_lock.release()
         print("Braking !!! Dangerously Close")
-        print(server_address)
+        #print(server_address)
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
         try:
@@ -156,7 +156,7 @@ def print_metrics(segments,distance,speed_lock):
         speed_lock.release()
         if slowdown.value != 2:
             print("Slowing down !!!")
-            print(server_address)
+            #print(server_address)
             client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             client_socket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
         try:
@@ -171,27 +171,27 @@ def print_metrics(segments,distance,speed_lock):
 
 
 def receiver(shared_gps,curr_ack,slowdown,received_ack,ack_lock,lock,speed_lock):
-    #server_ip = '10.192.241.2'
-    server_ip = '10.217.59.100'
+    server_ip = '10.114.241.208'
+    #server_ip = '10.192.240.106'
     server_port = 3000
     forward_train_gps = 0
-    train_id=12340
+    train_id=12345
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((server_ip, server_port))
-    print("binded")
+    #print("binded")
     while True:
         #sleep(0.2)
-        print("in while")
+        #print("in while")
         data, client_address = server_socket.recvfrom(1024)
-        print("received data")
+        #print("received data")
         segments = pickle.loads(data)
-        print(segments)
+        #print(segments)
         if len(segments)==1:
             ack_no=segments[0]
-            print("got ack")
-            print(ack_no)
+            #print("got ack")
+            #print(ack_no)
             ack_lock.acquire()
-            print(curr_ack.value)
+            #print(curr_ack.value)
             if ack_no==curr_ack.value:
                 received_ack.value=1
             ack_lock.release()
@@ -199,9 +199,9 @@ def receiver(shared_gps,curr_ack,slowdown,received_ack,ack_lock,lock,speed_lock)
             #segment data format [[gps],trackid,speed,train_no,sourceip,ackno]
             ack_message = [segments[-1]]
             ack_message.append(train_id)
-            print("train ack")
-            print(segments[-1])
-            print(ack_message)
+            #print("train ack")
+            #print(segments[-1])
+            #print(ack_message)
             ack_message = pickle.dumps(ack_message)
             client_ip, client_port = client_address
             client_port = 2000
@@ -215,7 +215,7 @@ def receiver(shared_gps,curr_ack,slowdown,received_ack,ack_lock,lock,speed_lock)
             print_metrics(segments,distance,speed_lock)
 
 def tcp_server(slowdown,speed_lock):
-    server_ip='10.217.59.100'
+    server_ip='10.114.241.208'
     server_port=2500
     server_address = (server_ip,server_port)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -225,9 +225,9 @@ def tcp_server(slowdown,speed_lock):
 
     while True:
         # Wait for a connection
-        print('Waiting for a connection...')
+        #print('Waiting for a connection...')
         client_socket, client_address = server_socket.accept()
-        print('Accepted connection from {}:{}'.format(*client_address))
+        #print('Accepted connection from {}:{}'.format(*client_address))
 
         # Receive and print the data
         data = client_socket.recv(1024)
